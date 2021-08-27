66ee63e097eb4f755469554fde8f93aa14384530
NineRogues <alexander.eric.3@gmail.com>
2021-08-27 17:03:10 -0500

    detail

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -38,8 +38,4 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def get_thumbnail(self):
-        if self.thumbnail:
-            return 'http://127.0.0.1:8000' + self.thumbnail.url
-        return ''
 
